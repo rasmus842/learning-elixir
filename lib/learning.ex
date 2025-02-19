3336cc4220a61d204dc697e4fef13cb917e5f306
Rasmus Zalite <rasmus842@localhost.localdomain>
2025-02-19 20:45:57 +0200

    Tests simple macro

--- lib/learning.ex
+++ lib/learning.ex
@@ -14,4 +14,13 @@ defmodule Learning do
   def hello do
     :world
   end
+
+  defmacro is_correct(num1, num2) do
+    quote do
+      unquote(num1) > unquote(num2)
+    end
+  end
+
+  def try_macro(num1, num2) when is_correct(num1, num2), do: "Correct!"
+  def try_macro(_, _), do: "Incorrect!"
 end
--- test/learning_test.exs
+++ test/learning_test.exs
@@ -1,8 +1,16 @@
 defmodule LearningTest do
   use ExUnit.Case
-  doctest Learning
+  import Learning
 
   test "greets the world" do
-    assert Learning.hello() == :world
+    assert hello() == :world
+  end
+
+  test "Testing macro" do
+    assert try_macro(5, 1) == "Correct!"
+    assert try_macro(1, 5) == "Incorrect!"
+
+    assert is_correct(5, 1) == true
+    assert is_correct(1, 5) == false
   end
 end
